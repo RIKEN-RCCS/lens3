fadf8dfe4c331ad18b5f4be8281133e4839717bd
zzmatu <zzmatu@users.noreply.github.com>
2023-05-27 00:01:12 +0900

    ()

--- v1/src/lenticularis/ui/assets/index.js
+++ v1/src/lenticularis/ui/assets/index.js
@@ -10795,264 +10795,17 @@ const VCard = genericComponent()({
     return {};
   }
 });
-const VGrid = "";
-const VContainer = genericComponent()({
-  name: "VContainer",
-  props: {
-    fluid: {
-      type: Boolean,
-      default: false
-    },
-    ...makeComponentProps(),
-    ...makeTagProps()
-  },
-  setup(props, _ref) {
-    let {
-      slots
-    } = _ref;
-    useRender(() => createVNode(props.tag, {
-      "class": ["v-container", {
-        "v-container--fluid": props.fluid
-      }, props.class],
-      "style": props.style
-    }, slots));
-    return {};
-  }
-});
-const breakpointProps = (() => {
-  return breakpoints.reduce((props, val) => {
-    props[val] = {
-      type: [Boolean, String, Number],
-      default: false
-    };
-    return props;
-  }, {});
-})();
-const offsetProps = (() => {
-  return breakpoints.reduce((props, val) => {
-    const offsetKey = "offset" + capitalize(val);
-    props[offsetKey] = {
-      type: [String, Number],
-      default: null
-    };
-    return props;
-  }, {});
-})();
-const orderProps = (() => {
-  return breakpoints.reduce((props, val) => {
-    const orderKey = "order" + capitalize(val);
-    props[orderKey] = {
-      type: [String, Number],
-      default: null
-    };
-    return props;
-  }, {});
-})();
-const propMap$1 = {
-  col: Object.keys(breakpointProps),
-  offset: Object.keys(offsetProps),
-  order: Object.keys(orderProps)
-};
-function breakpointClass$1(type, prop, val) {
-  let className = type;
-  if (val == null || val === false) {
-    return void 0;
-  }
-  if (prop) {
-    const breakpoint = prop.replace(type, "");
-    className += `-${breakpoint}`;
-  }
-  if (type === "col") {
-    className = "v-" + className;
-  }
-  if (type === "col" && (val === "" || val === true)) {
-    return className.toLowerCase();
-  }
-  className += `-${val}`;
-  return className.toLowerCase();
-}
-const ALIGN_SELF_VALUES = ["auto", "start", "end", "center", "baseline", "stretch"];
-const VCol = genericComponent()({
-  name: "VCol",
-  props: {
-    cols: {
-      type: [Boolean, String, Number],
-      default: false
-    },
-    ...breakpointProps,
-    offset: {
-      type: [String, Number],
-      default: null
-    },
-    ...offsetProps,
-    order: {
-      type: [String, Number],
-      default: null
-    },
-    ...orderProps,
-    alignSelf: {
-      type: String,
-      default: null,
-      validator: (str) => ALIGN_SELF_VALUES.includes(str)
-    },
-    ...makeComponentProps(),
-    ...makeTagProps()
-  },
-  setup(props, _ref) {
-    let {
-      slots
-    } = _ref;
-    const classes = computed(() => {
-      const classList = [];
-      let type;
-      for (type in propMap$1) {
-        propMap$1[type].forEach((prop) => {
-          const value = props[prop];
-          const className = breakpointClass$1(type, prop, value);
-          if (className)
-            classList.push(className);
-        });
-      }
-      const hasColClasses = classList.some((className) => className.startsWith("v-col-"));
-      classList.push({
-        // Default to .v-col if no other col-{bp}-* classes generated nor `cols` specified.
-        "v-col": !hasColClasses || !props.cols,
-        [`v-col-${props.cols}`]: props.cols,
-        [`offset-${props.offset}`]: props.offset,
-        [`order-${props.order}`]: props.order,
-        [`align-self-${props.alignSelf}`]: props.alignSelf
-      });
-      return classList;
-    });
-    return () => {
-      var _a;
-      return h(props.tag, {
-        class: [classes.value, props.class],
-        style: props.style
-      }, (_a = slots.default) == null ? void 0 : _a.call(slots));
-    };
-  }
-});
-const ALIGNMENT = ["start", "end", "center"];
-const SPACE = ["space-between", "space-around", "space-evenly"];
-function makeRowProps(prefix, def2) {
-  return breakpoints.reduce((props, val) => {
-    const prefixKey = prefix + capitalize(val);
-    props[prefixKey] = def2();
-    return props;
-  }, {});
-}
-const ALIGN_VALUES = [...ALIGNMENT, "baseline", "stretch"];
-const alignValidator = (str) => ALIGN_VALUES.includes(str);
-const alignProps = makeRowProps("align", () => ({
-  type: String,
-  default: null,
-  validator: alignValidator
-}));
-const JUSTIFY_VALUES = [...ALIGNMENT, ...SPACE];
-const justifyValidator = (str) => JUSTIFY_VALUES.includes(str);
-const justifyProps = makeRowProps("justify", () => ({
-  type: String,
-  default: null,
-  validator: justifyValidator
-}));
-const ALIGN_CONTENT_VALUES = [...ALIGNMENT, ...SPACE, "stretch"];
-const alignContentValidator = (str) => ALIGN_CONTENT_VALUES.includes(str);
-const alignContentProps = makeRowProps("alignContent", () => ({
-  type: String,
-  default: null,
-  validator: alignContentValidator
-}));
-const propMap = {
-  align: Object.keys(alignProps),
-  justify: Object.keys(justifyProps),
-  alignContent: Object.keys(alignContentProps)
-};
-const classMap = {
-  align: "align",
-  justify: "justify",
-  alignContent: "align-content"
-};
-function breakpointClass(type, prop, val) {
-  let className = classMap[type];
-  if (val == null) {
-    return void 0;
-  }
-  if (prop) {
-    const breakpoint = prop.replace(type, "");
-    className += `-${breakpoint}`;
-  }
-  className += `-${val}`;
-  return className.toLowerCase();
-}
-const VRow = genericComponent()({
-  name: "VRow",
-  props: {
-    dense: Boolean,
-    noGutters: Boolean,
-    align: {
-      type: String,
-      default: null,
-      validator: alignValidator
-    },
-    ...alignProps,
-    justify: {
-      type: String,
-      default: null,
-      validator: justifyValidator
-    },
-    ...justifyProps,
-    alignContent: {
-      type: String,
-      default: null,
-      validator: alignContentValidator
-    },
-    ...alignContentProps,
-    ...makeComponentProps(),
-    ...makeTagProps()
-  },
-  setup(props, _ref) {
-    let {
-      slots
-    } = _ref;
-    const classes = computed(() => {
-      const classList = [];
-      let type;
-      for (type in propMap) {
-        propMap[type].forEach((prop) => {
-          const value = props[prop];
-          const className = breakpointClass(type, prop, value);
-          if (className)
-            classList.push(className);
-        });
-      }
-      classList.push({
-        "v-row--no-gutters": props.noGutters,
-        "v-row--dense": props.dense,
-        [`align-${props.align}`]: props.align,
-        [`justify-${props.justify}`]: props.justify,
-        [`align-content-${props.alignContent}`]: props.alignContent
-      });
-      return classList;
-    });
-    return () => {
-      var _a;
-      return h(props.tag, {
-        class: ["v-row", classes.value, props.class],
-        style: props.style
-      }, (_a = slots.default) == null ? void 0 : _a.call(slots));
-    };
-  }
-});
-const VSpacer = createSimpleFunctional("flex-grow-1", "div", "VSpacer");
-const _hoisted_1$4 = { class: "text-body-1" };
-const _hoisted_2$2 = /* @__PURE__ */ createBaseVNode("li", null, "Lens3 is a multiplexer to MinIO to service multiple MinIO instances at a single access point. ", -1);
-const _hoisted_3$2 = /* @__PURE__ */ createBaseVNode("li", null, [
-  /* @__PURE__ */ createTextVNode("For the usage, see the Lens3 User Guide in: "),
+const _hoisted_1$4 = /* @__PURE__ */ createBaseVNode("li", null, " Lens3 is a multiplexer to MinIO to service multiple MinIO instances at a single access point. ", -1);
+const _hoisted_2$2 = /* @__PURE__ */ createBaseVNode("li", null, [
+  /* @__PURE__ */ createTextVNode(" For the usage, see the Lens3 User Guide in: "),
   /* @__PURE__ */ createBaseVNode("a", { href: "https://github.com/riken-rccs/lens3/" }, "github.com")
 ], -1);
 function _sfc_render$6(_ctx, _cache, $props, $setup, $data, $options) {
-  return openBlock(), createBlock(VCard, { "min-width": "300" }, {
+  return openBlock(), createBlock(VCard, {
+    variant: "outlined",
+    class: "text-body-1 pa-4",
+    "min-width": "300"
+  }, {
     default: withCtx(() => [
       createVNode(VCardTitle, null, {
         default: withCtx(() => [
@@ -11060,13 +10813,13 @@ function _sfc_render$6(_ctx, _cache, $props, $setup, $data, $options) {
         ]),
         _: 1
       }),
-      createVNode(VCardText, { class: "text-body-1 pa=1 ma=1" }, {
+      createVNode(VCardText, { class: "text-body-1" }, {
         default: withCtx(() => [
-          createBaseVNode("ul", _hoisted_1$4, [
+          createBaseVNode("ul", null, [
+            _hoisted_1$4,
             _hoisted_2$2,
-            _hoisted_3$2,
             createBaseVNode("li", null, [
-              createTextVNode("S3 is serviced at: " + toDisplayString($props.pool_data.s3_url) + " ", 1),
+              createTextVNode(" S3 is serviced at: " + toDisplayString($props.pool_data.s3_url) + " ", 1),
               createVNode(VBtn, {
                 icon: "mdi-clipboard-text",
                 variant: "plain",
@@ -11074,7 +10827,7 @@ function _sfc_render$6(_ctx, _cache, $props, $setup, $data, $options) {
               }, null, 8, ["onClick"]),
               createTextVNode(". ")
             ]),
-            createBaseVNode("li", null, "Lens3 version: " + toDisplayString($props.pool_data.lens3_version), 1)
+            createBaseVNode("li", null, " Lens3 version: " + toDisplayString($props.pool_data.lens3_version), 1)
           ])
         ]),
         _: 1
@@ -11089,8 +10842,7 @@ function _sfc_render$6(_ctx, _cache, $props, $setup, $data, $options) {
               createTextVNode(" Dismiss ")
             ]),
             _: 1
-          }),
-          createVNode(VSpacer)
+          })
         ]),
         _: 1
       })
@@ -11886,6 +11638,256 @@ const VAppBarTitle = genericComponent()({
     return {};
   }
 });
+const VGrid = "";
+const VContainer = genericComponent()({
+  name: "VContainer",
+  props: {
+    fluid: {
+      type: Boolean,
+      default: false
+    },
+    ...makeComponentProps(),
+    ...makeTagProps()
+  },
+  setup(props, _ref) {
+    let {
+      slots
+    } = _ref;
+    useRender(() => createVNode(props.tag, {
+      "class": ["v-container", {
+        "v-container--fluid": props.fluid
+      }, props.class],
+      "style": props.style
+    }, slots));
+    return {};
+  }
+});
+const breakpointProps = (() => {
+  return breakpoints.reduce((props, val) => {
+    props[val] = {
+      type: [Boolean, String, Number],
+      default: false
+    };
+    return props;
+  }, {});
+})();
+const offsetProps = (() => {
+  return breakpoints.reduce((props, val) => {
+    const offsetKey = "offset" + capitalize(val);
+    props[offsetKey] = {
+      type: [String, Number],
+      default: null
+    };
+    return props;
+  }, {});
+})();
+const orderProps = (() => {
+  return breakpoints.reduce((props, val) => {
+    const orderKey = "order" + capitalize(val);
+    props[orderKey] = {
+      type: [String, Number],
+      default: null
+    };
+    return props;
+  }, {});
+})();
+const propMap$1 = {
+  col: Object.keys(breakpointProps),
+  offset: Object.keys(offsetProps),
+  order: Object.keys(orderProps)
+};
+function breakpointClass$1(type, prop, val) {
+  let className = type;
+  if (val == null || val === false) {
+    return void 0;
+  }
+  if (prop) {
+    const breakpoint = prop.replace(type, "");
+    className += `-${breakpoint}`;
+  }
+  if (type === "col") {
+    className = "v-" + className;
+  }
+  if (type === "col" && (val === "" || val === true)) {
+    return className.toLowerCase();
+  }
+  className += `-${val}`;
+  return className.toLowerCase();
+}
+const ALIGN_SELF_VALUES = ["auto", "start", "end", "center", "baseline", "stretch"];
+const VCol = genericComponent()({
+  name: "VCol",
+  props: {
+    cols: {
+      type: [Boolean, String, Number],
+      default: false
+    },
+    ...breakpointProps,
+    offset: {
+      type: [String, Number],
+      default: null
+    },
+    ...offsetProps,
+    order: {
+      type: [String, Number],
+      default: null
+    },
+    ...orderProps,
+    alignSelf: {
+      type: String,
+      default: null,
+      validator: (str) => ALIGN_SELF_VALUES.includes(str)
+    },
+    ...makeComponentProps(),
+    ...makeTagProps()
+  },
+  setup(props, _ref) {
+    let {
+      slots
+    } = _ref;
+    const classes = computed(() => {
+      const classList = [];
+      let type;
+      for (type in propMap$1) {
+        propMap$1[type].forEach((prop) => {
+          const value = props[prop];
+          const className = breakpointClass$1(type, prop, value);
+          if (className)
+            classList.push(className);
+        });
+      }
+      const hasColClasses = classList.some((className) => className.startsWith("v-col-"));
+      classList.push({
+        // Default to .v-col if no other col-{bp}-* classes generated nor `cols` specified.
+        "v-col": !hasColClasses || !props.cols,
+        [`v-col-${props.cols}`]: props.cols,
+        [`offset-${props.offset}`]: props.offset,
+        [`order-${props.order}`]: props.order,
+        [`align-self-${props.alignSelf}`]: props.alignSelf
+      });
+      return classList;
+    });
+    return () => {
+      var _a;
+      return h(props.tag, {
+        class: [classes.value, props.class],
+        style: props.style
+      }, (_a = slots.default) == null ? void 0 : _a.call(slots));
+    };
+  }
+});
+const ALIGNMENT = ["start", "end", "center"];
+const SPACE = ["space-between", "space-around", "space-evenly"];
+function makeRowProps(prefix, def2) {
+  return breakpoints.reduce((props, val) => {
+    const prefixKey = prefix + capitalize(val);
+    props[prefixKey] = def2();
+    return props;
+  }, {});
+}
+const ALIGN_VALUES = [...ALIGNMENT, "baseline", "stretch"];
+const alignValidator = (str) => ALIGN_VALUES.includes(str);
+const alignProps = makeRowProps("align", () => ({
+  type: String,
+  default: null,
+  validator: alignValidator
+}));
+const JUSTIFY_VALUES = [...ALIGNMENT, ...SPACE];
+const justifyValidator = (str) => JUSTIFY_VALUES.includes(str);
+const justifyProps = makeRowProps("justify", () => ({
+  type: String,
+  default: null,
+  validator: justifyValidator
+}));
+const ALIGN_CONTENT_VALUES = [...ALIGNMENT, ...SPACE, "stretch"];
+const alignContentValidator = (str) => ALIGN_CONTENT_VALUES.includes(str);
+const alignContentProps = makeRowProps("alignContent", () => ({
+  type: String,
+  default: null,
+  validator: alignContentValidator
+}));
+const propMap = {
+  align: Object.keys(alignProps),
+  justify: Object.keys(justifyProps),
+  alignContent: Object.keys(alignContentProps)
+};
+const classMap = {
+  align: "align",
+  justify: "justify",
+  alignContent: "align-content"
+};
+function breakpointClass(type, prop, val) {
+  let className = classMap[type];
+  if (val == null) {
+    return void 0;
+  }
+  if (prop) {
+    const breakpoint = prop.replace(type, "");
+    className += `-${breakpoint}`;
+  }
+  className += `-${val}`;
+  return className.toLowerCase();
+}
+const VRow = genericComponent()({
+  name: "VRow",
+  props: {
+    dense: Boolean,
+    noGutters: Boolean,
+    align: {
+      type: String,
+      default: null,
+      validator: alignValidator
+    },
+    ...alignProps,
+    justify: {
+      type: String,
+      default: null,
+      validator: justifyValidator
+    },
+    ...justifyProps,
+    alignContent: {
+      type: String,
+      default: null,
+      validator: alignContentValidator
+    },
+    ...alignContentProps,
+    ...makeComponentProps(),
+    ...makeTagProps()
+  },
+  setup(props, _ref) {
+    let {
+      slots
+    } = _ref;
+    const classes = computed(() => {
+      const classList = [];
+      let type;
+      for (type in propMap) {
+        propMap[type].forEach((prop) => {
+          const value = props[prop];
+          const className = breakpointClass(type, prop, value);
+          if (className)
+            classList.push(className);
+        });
+      }
+      classList.push({
+        "v-row--no-gutters": props.noGutters,
+        "v-row--dense": props.dense,
+        [`align-${props.align}`]: props.align,
+        [`justify-${props.justify}`]: props.justify,
+        [`align-content-${props.alignContent}`]: props.alignContent
+      });
+      return classList;
+    });
+    return () => {
+      var _a;
+      return h(props.tag, {
+        class: ["v-row", classes.value, props.class],
+        style: props.style
+      }, (_a = slots.default) == null ? void 0 : _a.call(slots));
+    };
+  }
+});
+const VSpacer = createSimpleFunctional("flex-grow-1", "div", "VSpacer");
 const VMenu$1 = "";
 const VOverlay$1 = "";
 const makeDelayProps = propsFactory({
---
+++ v1/ui/src/components/About.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card variant="outlined" class="text-body-1 pa-4" min-width="300">
+    <v-card-title>About Lens3</v-card-title>
+    <v-card-text class="text-body-1">
+      <ul>
+        <li>
+      Lens3 is a multiplexer to MinIO to service multiple MinIO
+      instances at a single access point.
+        </li>
+        <li>
+      For the usage, see the Lens3 User Guide in:
+      <a href="https://github.com/riken-rccs/lens3/">github.com</a>
+        </li>
+        <li>
+      S3 is serviced at: {{pool_data.s3_url}}
+      <v-btn icon="mdi-clipboard-text" variant="plain"
+             v-on:click="kick_copy_url_to_clipboard" />.
+        </li>
+        <li>
+      Lens3 version: {{pool_data.lens3_version}}
+        </li>
+      </ul>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        variant="text"
+        v-on:click="pool_data.menu_visible=false" >
+        Dismiss
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    pool_data: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  //data() {
+  //  return {
+  //  };
+  //},
+  methods: {
+    kick_copy_url_to_clipboard() {
+      navigator.clipboard.writeText(this.pool_data.s3_url);
+    },
+  },
+}
+</script>
